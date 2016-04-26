71532c4ae5da4dfa35b0bde4fec1828655a134f6
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-04-25 23:16:16 -0300

    first commit

---
+++ edge.py
@@ -0,0 +1,8 @@
+from node import Node
+
+class Edge():
+	"""docstring for Edge"""
+	def __init__(self, arg):
+		super(Edge, self).__init__()
+		self.arg = arg
+		
---
+++ graph.py
@@ -0,0 +1,9 @@
+from node import Node
+from edge import Edge
+
+class Graph():
+	"""docstring for Graph"""
+	def __init__(self, arg):
+		super(Graph, self).__init__()
+		self.arg = arg
+		
