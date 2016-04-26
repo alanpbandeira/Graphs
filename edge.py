b926988f0c856569c47cb32457b4082c54172638
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-04-26 16:43:02 -0300

    init update

--- edge.py
+++ edge.py
@@ -2,7 +2,15 @@ from node import Node
 
 class Edge():
 	"""docstring for Edge"""
-	def __init__(self, arg):
-		super(Edge, self).__init__()
-		self.arg = arg
-		
+
+	incident_nodes = None
+
+	def __init__(self, e_id, weight, Node_a, Node_b):
+		self.e_id   = e_id
+		self.weight = weight
+		slef.setIncident(Node_a, Node_b)
+
+	def setIncident(self, Node_a, Node_b):
+		self.incident_nodes = (Node_a.index, Node_b.index)
+		Node_a.insertAdjacentNode(Node_b)
+		Node_b.insertAdjacentNode(Node_a)
--- node.py
+++ node.py
@@ -1,6 +1,20 @@
+from edge import Edge
+
 class Node():
 	"""docstring for Node"""
-	def __init__(self, arg):
-		super(Node, self).__init__()
-		self.arg = arg
+	
+	adjacencency_list = list()
+
+	def __init__(self, index):
+		self.index = index
+
+	def insertAdjacentNode(self, Node):
+		self.adjacencency_list.append(Node.index)
+
+	def removeAdjacentNode(self, Node):
+		self.adjacencency_list.remove(Node.index)
+		# Remover a aresta que conecta os dois vertices 
+
+
+
 		
