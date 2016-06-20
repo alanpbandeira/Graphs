208b29a8b75a4c3b5d4b253dd8d36ea37320c8c3
Alan Bandeira <alan.p.bandeira@gmail.com>
2016-06-20 07:27:12 -0300

    cost matrix update

--- graph.py
+++ graph.py
@@ -1,44 +1,63 @@
-import node
-import edge
+from node import Node
+from edge import Edge
 import random
+
 
 class Graph:
     """
 
     """
-    node_list = None
-    edge_list = None
+    node_list = {}
+    edge_list = []
+    incidence_list = []
 
     def __init__(self, file_name):
-            self.edgesFromFile(file_name)
+            self.graphFromFile(file_name)
 
-    def edgesFromFile(self, file_name):
+    def graphFromFile(self, file_name):
+        """
+        Instantiate a graph from a csv file
+        @:param file_name: String - name of the file to
+                be read. It must be an .csv file.
+        """
         path = 'data/' + file_name
-
         fhand = open(path, 'r')
 
         for line in fhand:
-            line = line.split(',')
-            self.edge_list.append(edge.Edge((line[0], line[1]), line[2]))
+            line = line.strip().split(',')
+            self.edge_list.append(Edge((line[0], line[1]), int(line[2])))
+            self.incidence_list.append((line[0], line[1]))
+
+        fhand.close()
+
+        fhand = open(path, 'r')
+        for line in fhand:
+            line = line.strip().split(',')
+
+            if line[0] not in self.node_list:
+                self.node_list[line[0]] = (Node(line[0], self.setNodeAdj(line[0])))
+            if line[1] not in self.node_list:
+                self.node_list[line[1]] = (Node(line[1], self.setNodeAdj(line[1])))
 
     def nodesFromEdges(self):
         for edge in self.edge_list:
+            pass
 
-
-    def setNodeAdj(self, elected_node):
+    def setNodeAdj(self, node_id):
         """
         Provides the adjacency list of a node from a list of edges.
-        :param elected_node:
-        :return a_list: a list of adjacent nodes of the elected_node
+        :param node_id: The id that identify the node from which the
+        adjacency list will be draw.
+        :return a_list: a list of adjacent nodes of the node_id
         """
         a_list = []
 
-        for t in self.edge_list:
-            if elected_node.node_id in t.incident_nodes:
-                if elected_node is t.incident_nodes[0]:
-                    a_list.append(t.incident_nodes[1])
+        for edge in self.edge_list:
+            if node_id in edge.incident_nodes:
+                if node_id is edge.incident_nodes[0]:
+                    a_list.append(edge.incident_nodes[1])
                 else:
-                    a_list.append(t.incident_nodes[0])
+                    a_list.append(edge.incident_nodes[0])
 
         return a_list
 
@@ -48,15 +67,42 @@ class Graph:
         :return mtx: A two dimensional array  info of the graph adjacency.
         """
         mtx = []
+        nodes = sorted(self.node_list)
 
-        for node_x in self.node_list:
+        for node_x in nodes:
             line = []
 
-            for node_y in self.node_list:
-                if node_y.node_id in node_x.adjacency_list:
+            for node_y in nodes:
+                if node_y in self.node_list[node_x].adjacency_list:
                     line.append(1)
                 else:
                     line.append(0)
+
+            mtx.append(line)
+
+        return mtx
+
+    def costMatrix(self, minimize=True):
+        """
+        Create the adjacency matrix of the graph
+        :return mtx: A two dimensional array  info of the graph adjacency.
+        """
+        mtx = []
+        nodes = sorted(self.node_list)
+
+        for node_x in nodes:
+            line = []
+
+            for node_y in nodes:
+                if (node_y, node_x) in self.incidence_list:
+                    line.append(self.edge_list[self.incidence_list.index((node_y, node_x))].weight)
+                elif (node_x, node_y) in self.incidence_list:
+                    line.append(self.edge_list[self.incidence_list.index((node_x, node_y))].weight)
+                else:
+                    if minimize:
+                        line.append(9999)
+                    else:
+                        line.append(-1)
 
             mtx.append(line)
 
--- node.py
+++ node.py
@@ -1,6 +1,11 @@
 class Node:
 
     def __init__(self, node_id, adjacency_list):
+        """
+        Defines the Node model for an graph.
+        :param node_id: String - Element that identify a node.
+        :param adjacency_list:
+        """
         self.node_id = node_id
         self.adjacency_list = adjacency_list
         self.degree = len(adjacency_list)
